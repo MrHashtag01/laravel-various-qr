be25a3d918c34651182c7cd0d27e168b217ea1f9
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-05-10 15:48:03 +0400

    Defined routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\QrCodeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/generate-qrcode', [QrCodeController::class, 'index']);
